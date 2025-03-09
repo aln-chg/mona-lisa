1111f400ecb8a162cacf99939339753e3cbfd575
vct-chg <vinceth.chang@gmail.com>
2025-03-09 10:37:52 +0800

    FIND financial report of TSMC

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Mona lisa is gay")
+import yfinance as yf
+
+stock_id = "2330.TW"
+stock = yf.Ticker(stock_id)
+
+financials = stock.financials
+balance_sheet = stock.balance_sheet
+cashflow = stock.cashflow
+
+print(financials)
+print(balance_sheet)
+print(cashflow)
